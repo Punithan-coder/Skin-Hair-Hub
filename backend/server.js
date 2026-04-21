7d176f788eb81fb7923aaf2d3570ab3754888133
Punithan-coder <punithsekarpunithsekar@gmail.com>
2026-04-21 21:18:13 +0530

    l

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Any request that doesn't match an API route will serve the React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
